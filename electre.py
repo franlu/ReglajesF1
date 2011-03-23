305192e229fa2a0fa62641014f2fc70e5244ed45
franlu <fran.lucena@gmail.com>
2011-03-23 11:54:53 +0100

    Added method of electre class

--- electre.py
+++ electre.py
@@ -20,28 +20,84 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
+from Math import sqrt
+
 class Electre:
 	
 	def __init__ (self,alternativas, atributos):
 		self.alternativas = alternativas
 		self.atributos = atributos
+		self.pesos = [0] * atributos
+		self.optimo = [0] * atributos
+		self.decisional = [[0] * atributos] * alternativas
+		self.normalizada = [[0] * atributos] * alternativas
+		self.ponderada = [[0] * atributos] * alternativas
+		self.concordada = [[0] * atributos] * alternativas
+		self.discordada = [[0] * atributos] * alternativas
+		self.dominancia = [[0] * atributos] * alternativas
+		
 		
 	def establecerPesos(self, prioridad):
 		
+		total = 0.0
+		for i in range(0, self.atributos):
+			total += prioridad[i]
+		for i in range(0, self.atributos):
+			self.pesos[i] = prioridad[i]
+		
 	def establecerOptimo(self, optimo):
 		
-	def establecerDecisional(self, decisional):
+		for i in range(0, self.atributos):
+			self.optimo[i] = optimo[i]
+					
+	def __establecerDecisional(self, decisional):
 		
-	def establecerNormalizada():
+		for i in range(0, self.alternativas):
+			for j in range(0, self.atributos):
+				self.decisional[i][j] = decisional[i][j]		
+	
+	def establecerNormalizada(self):
 		
-	def establecerPonderada():
+		for j in range(0, self.atributos):
+			suma = 0
+			for i in range(0, self.alternativas):
+				cuadrado = self.decisional[i][j] * self.decisional[i][j]
+				suma = suma + cuadrado
+			
+			suma = sqrt(suma)
+			for i in range(0, self.alternativas):
+				self.normalizada[i][j] = (self.decisional[i][j] / suma)
+			
+	
+	def establecerPonderada(self):
 		
-	def establecerConcordada():
+		for i in range(0, self.alternativas):
+			for j in range(0, self.atributos):
+				self.ponderada[i][j] = self.normalizada[i][j] * self.pesos[j]
+				
+	def establecerConcordada(self):
+		print ""
+	
+	def establecerDiscordada(self):
+		print ""
+	
+	def establecerDominada(self):
+		print ""
+	
+	def resolver(self, decisional, prioridad, optimo):
 		
-	def establecerDiscordada():
+		self.establecerPesos(prioridad)
+		self.establecerOptimo(optimo)
+		self.establecerDecisional(decisional)
+		self.establecerNormalizada()
+		self.establecerPonderada()
+		self.establecerConcordada()
+		self.establecerConcordada()
+		self.establecerDominada()
 		
-	def establecerDominada():
+	def mostrar(self, matriz):
 		
-	def resolver(decisional, prioridad, optimo):
-		
-	def mostrar(double):
+		for i in range(0, self.alternativas):
+			for j in range(0, self.atributos):
+				print matriz[i][j],
+			print			
